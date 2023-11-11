007b333591778b98b23c3ea8f5060a2feb845c96
github-username <bhuyn053@ucr.edu>
2023-11-10 21:38:16 -0800

    add database

--- main.cpp
+++ main.cpp
@@ -6,20 +6,8 @@ using namespace std;
 
 int main () {
 
-    /*
-    Food first = Food("beef", "meat", 141, 9);
-
-    cout << first.getName() << endl;
-    cout << first.getType() << endl;
-    cout << first.getID() << endl;
-    cout << first.getQTY() << endl;
-    first.reduceQTY(2);
-    */
-
     Food* testBase = catalog::generateCatalog();
-    /*
-    cout << "testBase: " << testBase[0].getName() << endl;
-    */
+    
 
     for(int i = 0; i < 32; i++) {
 
