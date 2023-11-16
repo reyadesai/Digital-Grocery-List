120a38e8d4f9c58caf53f5b32d94aff163e64f2f
github-username <bhuyn053@ucr.edu>
2023-11-15 21:20:10 -0800

    tweak

--- code/main.cpp
+++ code/main.cpp
@@ -6,16 +6,6 @@ using namespace std;
 
 int main() {
     Food* testBase = catalog::generateCatalog();
-    
-
-    for(int i = 0; i < 33; i++) {
-
-        cout << testBase[i].getName() << " : " << testBase[i].getType() << " : " << testBase[i].getID() << " : " << testBase[i].getQTY() << " : " << testBase[i].getPrice() << endl;
-
-
-    }
- //free memory
-    delete[] testBase;
 
     /*
     0.
